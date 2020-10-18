f1054faf938720932d2f07cc04905aa0377e2def
el mehdi rhindi <g6handi@gmail.com>
2020-10-18 14:40:19 +0200

    code refactor

--- src/index.js
+++ src/index.js
@@ -1,68 +1,60 @@
-import {covidLayers} from './layers'
-import {GoogleMapDeckGL} from './google-map-deckgl'
+import { covidLayers } from './layers'
+import { GoogleMapDeckGL } from './google-map-deckgl'
 
 class App {
-  constructor() {
-    this.GoogleMapDeckGL = new GoogleMapDeckGL()
-    this.init()
-    this.selectedButton
-  }
+	constructor() {
+		this.GoogleMapDeckGL = new GoogleMapDeckGL()
+		this.init()
+		this.selectedButton
+	}
 
-  async init() {  
-    const defaultId = 0
-    const defaultLayers = covidLayers[defaultId]
-    const map_options = defaultLayers.getMapOptions()
-    await this.GoogleMapDeckGL.initMapWithOverlay(map_options)    
-    this.buildMenu(defaultId)
-    this.changeExample(defaultLayers)
-  }
+	async init() {
+		const defaultId = 0
+		const defaultLayers = covidLayers[defaultId]
+		const map_options = defaultLayers.getMapOptions()
+		await this.GoogleMapDeckGL.initMapWithOverlay(map_options)
+		this.buildMenu(defaultId)
+		this.changeExample(defaultLayers)
+	}
 
-  buildMenu(defaultLayersId) {
-    const menu = document.getElementById('menu')
-    this.GoogleMapDeckGL.map.addListener('dragstart', () => {      
-      menu.classList.add('fade')
-    })    
-    this.GoogleMapDeckGL.map.addListener('dragend', () => {
-      menu.classList.remove('fade')
-    })
-    covidLayers.forEach((example, index) => {
-      const exampleMetadata = example.getMetadata()
-      const button = document.createElement('button')
-      const label = document.createElement('div')
-      label.classList.add('label')
-      label.innerText = exampleMetadata.name
-      button.style.backgroundImage = `url(${exampleMetadata.thumbnail})`
-      button.onclick = (() => {                 
-        this.selectedButton.classList.remove('selected')    
-        this.selectedButton = button
-        this.selectedButton.classList.add('selected')
-        this.changeExample(example)
-      }).bind(this, example)
-      button.appendChild(label)
-      menu.appendChild(button)
-      if (index === defaultLayersId) {
-        this.selectedButton = button
-        this.selectedButton.classList.add('selected')
-      }      
-    })
-  }
+	buildMenu(defaultLayersId) {
+		const menu = document.getElementById('menu')
+		this.GoogleMapDeckGL.map.addListener('dragstart', () => {
+			menu.classList.add('fade')
+		})
+		this.GoogleMapDeckGL.map.addListener('dragend', () => {
+			menu.classList.remove('fade')
+		})
+		covidLayers.forEach((example, index) => {
+			const exampleMetadata = example.getMetadata()
+			const button = document.createElement('button')
+			const label = document.createElement('div')
+			label.classList.add('label')
+			label.innerText = exampleMetadata.name
+			button.style.backgroundImage = `url(${exampleMetadata.thumbnail})`
+			button.onclick = (() => {
+				this.selectedButton.classList.remove('selected')
+				this.selectedButton = button
+				this.selectedButton.classList.add('selected')
+				this.changeExample(example)
+			}).bind(this, example)
+			button.appendChild(label)
+			menu.appendChild(button)
+			if (index === defaultLayersId) {
+				this.selectedButton = button
+				this.selectedButton.classList.add('selected')
+			}
+		})
+	}
 
-  changeExample(newLayers) {
-    const layers = newLayers.getLayers()    
-    const mapOptions = newLayers.getMapOptions()
-    setTimeout(()=>{
-      this.setLayer(layers)
-      this.GoogleMapDeckGL.setMap(mapOptions)
-    },100)
-    
-  }
-
-  async setLayer(layers) {
-    let next = await layers.next()
-    if (next.value){
-      this.GoogleMapDeckGL.setLayer(next.value)
-    }
-  }  
+	async changeExample(newLayers) {
+		const layers = await newLayers.getLayers()
+		const mapOptions = newLayers.getMapOptions()
+		setTimeout(() => {
+			this.GoogleMapDeckGL.setLayer(layers)
+			this.GoogleMapDeckGL.setMap(mapOptions)
+		}, 100)
+	}
 }
 
 const app = new App()
--- src/layers/trackCoronaScatterplot/index.js
+++ src/layers/trackCoronaScatterplot/index.js
@@ -1,64 +1,71 @@
-import {ScatterplotLayer} from '@deck.gl/layers'
+import { ScatterplotLayer } from '@deck.gl/layers'
 
 export class TrackCoronaScatterplotLayer {
-  constructor(url, metaData, id, mapOptions) {
-      this.url = url
-      this.metaData = metaData
-      this.id = id
-      this.mapOptions = mapOptions
-  }
-  async *getLayers() {    
-    const data = await fetch(this.url).then(res => res.json()).then(data => data.data)
+    data
 
-    const layers = [
-      new ScatterplotLayer({
-        id: this.id,
-        data: data,
-        opacity: 0.8,
-        filled: true,
-        radiusMinPixels: 10,
-        radiusMaxPixels: 40,
-        pickable: true,
-        autoHighlight: true,
-        getPosition: d => [d.longitude, d.latitude],
-        getRadius: d => d.confirmed,
-        getFillColor: [243, 119, 72],
-        onHover: ({ object, x, y }) => {
-            const tooltipEl = document.getElementById('tooltip')
-            const locationEl = document.getElementById('location')
-            const updatedEl = document.getElementById('updated')
-            const confirmedEl = document.getElementById('confirmed')
-            const recoveredEl = document.getElementById('recovered')
-            const deadEl = document.getElementById('dead')
-            if (object) {
-                const {location, updated, confirmed, recovered, dead} = object
-                locationEl.textContent = location
-                updatedEl.textContent = updated
-                confirmedEl.textContent = confirmed
-                recoveredEl.textContent = recovered
-                deadEl.textContent = dead
-                tooltipEl.style.zIndex = 3
-                tooltipEl.style.left = x + 10 + 'px'
-                tooltipEl.style.top = y + 10 + 'px'
-            } else {
-              tooltipEl.style.zIndex = 0
-            }
+    constructor(url, metaData, id, mapOptions) {
+        this.url = url
+        this.metaData = metaData
+        this.id = id
+        this.mapOptions = mapOptions
+        this.getData()
+    }
+
+    async getData() {
+        this.data = await fetch(this.url).then(res => res.json()).then(data => data.data)
+    }
+
+    async getLayers() {
+        const layers = [
+            new ScatterplotLayer({
+                id: this.id,
+                data: this.data,
+                opacity: 0.8,
+                filled: true,
+                radiusMinPixels: 10,
+                radiusMaxPixels: 40,
+                pickable: true,
+                autoHighlight: true,
+                getPosition: d => [d.longitude, d.latitude],
+                getRadius: d => d.confirmed,
+                getFillColor: [243, 119, 72],
+                onHover: ({ object, x, y }) => {
+                    const tooltipEl = document.getElementById('tooltip')
+                    const locationEl = document.getElementById('location')
+                    const updatedEl = document.getElementById('updated')
+                    const confirmedEl = document.getElementById('confirmed')
+                    const recoveredEl = document.getElementById('recovered')
+                    const deadEl = document.getElementById('dead')
+                    if (object) {
+                        const { location, updated, confirmed, recovered, dead } = object
+                        locationEl.textContent = location
+                        updatedEl.textContent = updated
+                        confirmedEl.textContent = confirmed
+                        recoveredEl.textContent = recovered
+                        deadEl.textContent = dead
+                        tooltipEl.style.zIndex = 3
+                        tooltipEl.style.left = x + 10 + 'px'
+                        tooltipEl.style.top = y + 10 + 'px'
+                    } else {
+                        tooltipEl.style.zIndex = 0
+                    }
+                }
+            })
+        ]
+        return layers
+    }
+
+    getMapOptions() {
+        return {
+            center: this.mapOptions.center,
+            zoom: this.mapOptions.zoom
         }
-      })
-    ]
-
-    return layers
-  }
-  getMapOptions() {
-    return {
-      center: this.mapOptions.center,
-      zoom: this.mapOptions.zoom
     }
-  }
-  getMetadata() {
-    return {
-      name: this.metaData.name,
-      thumbnail: this.metaData.img
+    
+    getMetadata() {
+        return {
+            name: this.metaData.name,
+            thumbnail: this.metaData.img
+        }
     }
-  }
 }
